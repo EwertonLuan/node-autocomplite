6a83e849ad3c485939d5395697487dbd750dbc2f
Ewerton Luan <30380940+EwertonLuan@users.noreply.github.com>
2018-11-18 21:08:49 -0200

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -2,9 +2,9 @@ const config = {};
 
 
 /**Link mongoose, if you will go use mongodb online or local put your URL here*/
-config.MONGOOSE_URL = 'mongodb://dito:Hurley$123@ds151943.mlab.com:51943/dito';
+config.MONGOOSE_URL = 'mongodb://localhost:27017/dito';
 
-config.MONGOOSE_URL_TEST= 'mongodb://dito:Hurley$123@ds143330.mlab.com:43330/dito_test';
+config.MONGOOSE_URL_TEST= 'mongodb://localhost:27017/dito_test';
 
 config.PORT= 4000;
 
